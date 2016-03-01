f0f561dcdfb7c19e9c25dd88de8bf3fe725afbbc
PlinioDiversi <pliniodi@terra.com.br>
2016-03-01 19:10:34 -0300

    Inicial

---
+++ Metodos.LojaVirtual.UnitTest/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Metodos.LojaVirtual.UnitTest
+{
+    [TestClass]
+    public class UnitTestMetodos
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
